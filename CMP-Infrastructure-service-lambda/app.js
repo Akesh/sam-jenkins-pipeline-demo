adb2ac5e232c275d51441739cad7c0a9c4ba2778
akesh.patil <akesh.patil@BCT-LTP-G399.blazeclan.in>
2022-04-25 18:44:38 +0530

    Merged DEV into PROD

--- CMP-Infrastructure-service-lambda/app.js
+++ CMP-Infrastructure-service-lambda/app.js
@@ -21,7 +21,11 @@ exports.lambdaHandler = async (event, context) => {
     response = {
       statusCode: 200,
       body: JSON.stringify({
+<<<<<<< HEAD
         message: "This is PROD branch",
+=======
+        message: "This is DEV branch",
+>>>>>>> refs/remotes/origin/DEV
         // location: ret.data.trim()
       }),
     };
